c955f033e1cb4d0dface879a40dba206ca8c1976
Kaesoron <postal0055@gmail.com>
2023-04-09 17:13:59 +0300

    Correction for NumberFormatError message at Order assign

--- CarService/src/main/java/org/example/service/ConsoleProcessor.java
+++ CarService/src/main/java/org/example/service/ConsoleProcessor.java
@@ -210,7 +210,7 @@ public class ConsoleProcessor {
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Incorrect command: not enough arguments");
                 } catch (NumberFormatException e) {
-                    System.out.println("Incorrect Order ID number of Repairer ID number");
+                    System.out.println("Incorrect Order, Garage or Repairer ID number");
                 } catch (NullPointerException e) {
                     System.out.println("Incorrect Order ID");
                 }
