23bf071a6cb99a38dcc498aed8b65b23fab1542b
neiroblaster <neiroblaster@gmail.com>
2023-04-09 20:16:35 +0300

    ConsoleProcessor returned trim by regex

--- CarService/src/main/java/org/example/service/ConsoleProcessor.java
+++ CarService/src/main/java/org/example/service/ConsoleProcessor.java
@@ -66,8 +66,7 @@ public class ConsoleProcessor {
                 System.out.println("=====");
                 System.out.println("Please enter command or type \"help\" for command list. Enter \"exit\" to exit:");
 
-                input = reader.readLine().toLowerCase().trim();
-//                        .replaceAll("[^\\da-zA-Zа-яёА-ЯЁ ]", "");
+                input = reader.readLine().toLowerCase().trim().replaceAll("[^\\da-zA-Zа-яёА-ЯЁ ]", "");
 
                 if (input.equals("exit")) {
                     break;
